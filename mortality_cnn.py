2ed96b70719e861a7ad428b9adc651e15d6ef470
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2023-06-06 19:36:12 +0100

    Update mortality_cnn.py

--- mortality_cnn.py
+++ mortality_cnn.py
@@ -7,31 +7,72 @@ import tf_cnns
 import utils
 import argparse
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.optimizers import Adam, SGD, RMSprop    
-from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard, Callback
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam, SGD, RMSprop
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard, Callback, LearningRateScheduler
 from datetime import datetime
 from keras import backend as K
 from keras.losses import BinaryCrossentropy, SparseCategoricalCrossentropy, CategoricalCrossentropy, losses_utils
 from sklearn.utils import class_weight
 from tensorflow.keras.regularizers import l2
 import tempfile
+from array import array
 from keras import Model, Sequential
 from keras.layers import Dropout, Flatten, Dense, concatenate, add, Input, Conv2D, MaxPool2D, BatchNormalization, \
     AveragePooling2D, GlobalAveragePooling2D, Activation, ZeroPadding2D
+import cv2
+from keras.models import load_model
 
 # Command line arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--directory", required=True, help="path to input directory")
 ap.add_argument("-t", "--target", required=True, help="name of the target field")
 args = vars(ap.parse_args())
 
 # Set parameters
 INPUT_DIM = 224
 WIDTH = 224
-HEIGHT = 672
-BATCH_SIZE = 64
+HEIGHT = 1344
+BATCH_SIZE = 32
 NUM_EPOCHS = 500
 N_CLASSES = 1
+CHECKPOINT_PATH = os.path.join("model_weights", "cp-{epoch:02d}")
+
+# ''' Fine tuning step - VGG19 and ResNet50'''
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+from keras.applications.vgg16 import VGG16
+from keras.applications.resnet import ResNet50
+
+#model = VGG16(include_top=False, input_shape=(1344, 224, 3), weights='imagenet')
+model = ResNet50(include_top=False, input_shape=(1344, 224, 3), weights='imagenet')
+
+#transfer_layer = model.get_layer('block5_pool')
+#vgg_model = Model(inputs=model.input, outputs=transfer_layer.output)
+transfer_layer = model.get_layer('conv5_block3_out')
+resnet_model = Model(inputs=model.input, outputs=transfer_layer.output)
+
+for layer in resnet_model.layers:
+    layer.trainable = False
+
+#for layer in vgg_model.layers[0:17]:
+#    layer.trainable = False
+#my_model = Sequential()
+#my_model.add(vgg_model)
+my_model = Sequential()
+my_model.add(resnet_model)
+my_model.add(Flatten())
+my_model.add(Dropout(0.5))
+my_model.add(Dense(1024, activation='relu'))
+my_model.add(Dropout(0.5))
+my_model.add(Dense(512, activation='relu'))
+my_model.add(Dropout(0.5))
+my_model.add(Dense(1, activation='sigmoid'))
 
 gpus = tf.config.list_physical_devices('GPU')
 if gpus:
@@ -40,20 +81,29 @@ if gpus:
                                                                       (memory_limit=4096)])
 
 # Info file
-label_file = pd.read_csv('/Users/ebrahamalskaf/Documents/Labels.csv')
+label_file = pd.read_csv('/Users/ebrahamalskaf/Documents/final_surv.csv')
 
 # Loading images and labels
-(df) = utils.load_image_png(args["directory"], label_file, INPUT_DIM)
+(df1) = utils.load_label_png('/Users/ebrahamalskaf/Documents/**PERFUSION_CLASSIFICATION**/peak_LV_images', label_file, INPUT_DIM)
+print(len(df1))
+(df2) = utils.load_lge_data('/Users/ebrahamalskaf/Documents/**LGE_CLASSIFICATION**/lge_img', label_file, INPUT_DIM)
+print(len(df2))
+df = df1.merge(df2, on='ID')
+print(len(df))
 
-# class_weight = class_weight.compute_class_weight('balanced', classes=np.unique(labels), y=labels)
-# print(class_weight)
-class_weight = {0: 0.53042993,
-                1: 8.71559633}
+class_weight = {0: 0.595,
+                1: 3.3130}
 
 # Splitting data
 (df_train, df_valid) = train_test_split(df, train_size=0.7, stratify=df[args["target"]])
-X_train = np.array([x for x in df_train['images']]) / 255.0
-X_valid = np.array([z for z in df_valid['images']]) / 255.0
+X_train1 = np.array([x1 / 255.0 for x1 in df_train['Perf']])
+X_train2 = np.array([x2 / 255.0 for x2 in df_train['LGE']])
+X_train = np.hstack((X_train1, X_train2))
+print(X_train.shape)
+X_valid1 = np.array([x1 / 255.0 for x1 in df_valid['Perf']])
+X_valid2 = np.array([x2 / 255.0 for x2 in df_valid['LGE']])
+X_valid = np.hstack((X_valid1, X_valid2))
+
 y_train = np.array(df_train.pop(args["target"]))
 tlist = y_train.tolist()
 print(tlist.count(1))
@@ -63,11 +113,18 @@ print(vlist.count(1))
 print(y_train[:10])
 print(y_valid[:10])
 
+# Data augmentation
+aug = ImageDataGenerator(samplewise_center=True, samplewise_std_normalization=True, rotation_range=20,
+                         width_shift_range=0.1,
+                         height_shift_range=0.1, shear_range=0.2, zoom_range
+                         =0.2, horizontal_flip=True, fill_mode="nearest")
+
+v_aug = ImageDataGenerator(samplewise_center=True, samplewise_std_normalization=True)
+
 # Initialise the optimiser and model
 print("[INFO] compiling model ...")
 Opt = Adam(lr=0.001)
 Loss = BinaryCrossentropy()
-
 METRICS = [
     tf.keras.metrics.TruePositives(name='tp'),
     tf.keras.metrics.FalsePositives(name='fp'),
@@ -80,6 +137,7 @@ METRICS = [
     tf.keras.metrics.AUC(name='prc', curve='PR'),  # precision-recall curve
 ]
 
+# image_model = tf_cnns.LeNet.build(WIDTH, HEIGHT, depth=1, classes=N_CLASSES)
 def build_model(output_bias=None, reg=0.0002):
     if output_bias is not None:
         output_bias = tf.keras.initializers.Constant(output_bias)
@@ -90,24 +148,59 @@ def build_model(output_bias=None, reg=0.0002):
     # if we are using "channels first", update the input shape
     # and channels dimension
     if K.image_data_format() == "channels_first":
-        inputShape = (1, HEIGHT, WIDTH)
+        inputShape = (depth, height, width)
         chanDim = 1
 
-    # first set of CONV => RELU => POOL layers
-    model.add(Conv2D(20, (5, 5), padding="same",
-                         input_shape=inputShape))
+    # Block #1: first CONV => RELU => POOL layer set
+    model.add(Conv2D(96, (11, 11), strides=(4, 4),
+                     input_shape=inputShape, padding="same",
+                     kernel_regularizer=l2(reg)))
     model.add(Activation("relu"))
-    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(BatchNormalization(axis=chanDim))
+    model.add(MaxPool2D(pool_size=(3, 3), strides=(2, 2)))
+    model.add(Dropout(0.25))
 
-    # second set of CONV => RELU => POOL layers
-    model.add(Conv2D(50, (5, 5), padding="same"))
+    # Block #2: second CONV => RELU => POOL layer set
+    model.add(Conv2D(256, (5, 5), padding="same",
+                     kernel_regularizer=l2(reg)))
     model.add(Activation("relu"))
-    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(BatchNormalization(axis=chanDim))
+    model.add(MaxPool2D(pool_size=(3, 3), strides=(2, 2)))
+    model.add(Dropout(0.25))
 
-    # first set of FC => RELU layers
+    # Block #3: CONV => RELU => CONV => RELU => CONV => RELU
+    model.add(Conv2D(384, (3, 3), padding="same",
+                     kernel_regularizer=l2(reg)))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(axis=chanDim))
+    model.add(Conv2D(384, (3, 3), padding="same",
+                     kernel_regularizer=l2(reg)))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(axis=chanDim))
+    model.add(Conv2D(256, (3, 3), padding="same",
+                     kernel_regularizer=l2(reg)))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(axis=chanDim))
+    model.add(MaxPool2D(pool_size=(3, 3), strides=(2, 2)))
+    model.add(Dropout(0.25))
+
+    # Block #4: first set of FC => RELU layers
     model.add(Flatten())
-    model.add(Dense(20, kernel_regularizer=l2(0.0001)))
+    model.add(Dense(4096, kernel_regularizer=l2(reg)))
     model.add(Activation("relu"))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+
+    # Block #5: second set of FC => RELU layers
+    model.add(Dense(4096, kernel_regularizer=l2(reg)))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+
+    model.add(Dense(100))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
 
     # sigmoid classifier
     model.add(Dense(N_CLASSES, activation='sigmoid', bias_initializer=output_bias))
@@ -119,25 +212,25 @@ def build_model(output_bias=None, reg=0.0002):
 
     return model
 
-
-weigth_path = "{}_my_model.best.hdf5".format("image_mortality_LeNet")
-checkpoint = ModelCheckpoint(weigth_path, monitor='val_prc', save_best_only=True, mode='max', save_weights_only=False)
-
+weigth_path = "{}_my_model.best.hdf5".format("image_mortality_AlexNet")
+checkpoint = ModelCheckpoint(weigth_path, monitor='val_loss', save_best_only=True, mode='min', save_weights_only=False)
+def scheduler(epoch, lr):
+  if epoch < 10:
+    return lr
+  else:
+    return lr * tf.math.exp(-0.1)
 early_stopping = tf.keras.callbacks.EarlyStopping(
-    monitor='val_prc',
+    monitor='val_loss',
     verbose=1,
-    patience=30,
-    mode='max',
+    patience=20,
+    mode='min',
     restore_best_weights=True)
+callback = LearningRateScheduler(scheduler)
 
-image_model = build_model()
-print(image_model.predict(X_train[:10]))
-
-initial_bias = np.log([0.053])
+initial_bias = np.log([0.08])
 model = build_model(output_bias=initial_bias)
 initial_weights = os.path.join(tempfile.mkdtemp(), 'initial_weights')
 model.save_weights(initial_weights)
-
 image_model = build_model()
 image_model.load_weights(initial_weights)
 
@@ -146,25 +239,25 @@ tensorboard_callback = TensorBoard(log_dir=logdir, histogram_freq=1)
 
 # Training the model
 # print("[INFO] Training the model ...")
-
-history = image_model.fit(X_train, y_train, validation_data=(X_valid, y_valid), batch_size=BATCH_SIZE,
+image_model.compile(loss= Loss, optimizer=Opt, metrics=METRICS)
+history = image_model.fit_generator(aug.flow(X_train, y_train, batch_size=BATCH_SIZE), validation_data=v_aug.flow(X_valid, y_valid),
                           epochs=NUM_EPOCHS,
                           callbacks=[early_stopping, checkpoint, tensorboard_callback], class_weight=class_weight)
 
 # summarize history for loss
-plt.plot(history.history['precision'])
-plt.plot(history.history['val_precision'])
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.title('Mortality CNN training')
-plt.ylabel('precision')
+plt.ylabel('accuracy_loss')
 plt.xlabel('epoch')
-plt.legend(['train precision', 'validation precision', 'train loss', 'validation loss'], loc='upper left')
+plt.legend(['train accuracy', 'validation accuracy', 'train loss', 'validation loss'], loc='upper left')
 plt.show()
 
 # Saving model data
 model_json = image_model.to_json()
-with open("image_mortality_LeNet.json", "w") as json_file:
+with open("image_mortality_AlexNet.json", "w") as json_file:
     json_file.write(model_json)
 
 K.clear_session()
